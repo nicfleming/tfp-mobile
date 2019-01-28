536b621b3cce09fbb53d16d59e415f6c29bf2469
Nick Fleming <nicfleming@deloitte.com>
2019-01-27 22:36:56 -0500

    Register page home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  registerPage = RegisterPage;
+  
   constructor(public navCtrl: NavController) {
 
   }
